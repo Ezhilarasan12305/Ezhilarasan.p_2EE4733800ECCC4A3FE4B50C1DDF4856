6f9d35454e437a0704e4b5ad7bc51c3a9e72a099
Ezhilarasan12305 <144415467+Ezhilarasan12305@users.noreply.github.com>
2023-09-08 11:25:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def isLeapYear(year):
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
 
-   if (year%4==0.and year %100!=0) or year %100==0:
-     return True
-   else:
-     return False
-year = int(input("Enter a year : "))
-if isLeapYear(year):
- 
-  print("{} is a leap year.".format(year))
-else:
-  print("{} is not a leap year.".format(year))
+
+number = int(input("Enter a value :"))
+res = fact_rec(number)
+
+print("The factorial of {}is{}.".format(number,res))
